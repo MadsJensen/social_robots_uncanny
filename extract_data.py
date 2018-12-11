a3344adf8fcb64c8bca28d9b112b3e3abdc1eabb
Mads Jensen <mje.mads@gmail.com>
2018-12-11 15:27:11 +0100

    ENH: added robot as factor

--- extract_data.py
+++ extract_data.py
@@ -12,7 +12,8 @@ chs = [
     'CP1', 'CP5', 'CP2', 'CP6', 'Pz', 'P3', 'P7', 'P4', 'p8', 'Oz', 'O1', 'O2'
 ]
 conditions = ['1', '2', '3', '4', '5']
-times = [0.109, 0.232, 0.420]  # times to extract
+robots = ['A', 'B', 'C', 'D']
+times = [0.109, 0.232, 0.413]  # times to extract
 
 epochs = mne.read_epochs(data_path + "%s-epo.fif" % subject)
 epochs.set_eeg_reference()
@@ -24,16 +25,17 @@ results = pd.DataFrame()
 for tt in times:
     epochs_crop = epochs.copy()
     epochs_crop.crop(tt - 0.01, tt + 0.01)
-
-    for condition in conditions:
-        tmp = epochs_crop[condition]
-        for jj in range(len(tmp)):
-            row = pd.DataFrame([{
-                'condition': condition,
-                'value': tmp[jj].get_data().mean(),
-                'time': tt,
-                'subject': subject
-            }])
-            results = pd.concat((results, row))
+    for robot in robots:
+        for condition in conditions:
+            tmp = epochs_crop[robot + '/' + condition]
+            for jj in range(len(tmp)):
+                row = pd.DataFrame([{
+                    'condition': condition,
+                    'value': tmp[jj].get_data().mean(),
+                    'robot': robot,
+                    'time': tt,
+                    'subject': subject
+                }])
+                results = pd.concat((results, row))
 
 results.to_csv(data_path + 'results_%s.csv' % subject, index=False)
